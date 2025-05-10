8a7bb14c2cb03a24373c503217ba4674ea6e2961
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-05-10 18:26:59 +0300

    feat: Teacher Model Created

---
+++ src/main/java/gr/aueb/cf/schoolapp/model/AbstractEntity.java
@@ -0,0 +1,36 @@
+package gr.aueb.cf.schoolapp.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.ws.rs.GET;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class AbstractEntity {
+
+    @Column(name = "createdAt", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updatedAt", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(unique = true, updatable = false, nullable = false, length = 36)
+    private String uuid = UUID.randomUUID().toString();
+
+    @PrePersist
+    protected void onCreate(){
+        if(createdAt == null) createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    protected void onUpdate(){
+        updatedAt = LocalDateTime.now();
+    }
+}
