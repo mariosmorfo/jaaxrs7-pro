83f1eecdc2bd99a33c01afc496f191570163c16b
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-05-15 20:17:28 +0300

    Completed Teacher Controller

--- src/main/java/gr/aueb/cf/schoolapp/rest/TeacherRestController.java
+++ src/main/java/gr/aueb/cf/schoolapp/rest/TeacherRestController.java
@@ -3,9 +3,8 @@ package gr.aueb.cf.schoolapp.rest;
 import gr.aueb.cf.schoolapp.core.exceptions.EntityAlreadyExistsException;
 import gr.aueb.cf.schoolapp.core.exceptions.EntityInvalidArgumentException;
 import gr.aueb.cf.schoolapp.core.exceptions.EntityNotFoundException;
-import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
-import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
-import gr.aueb.cf.schoolapp.dto.TeacherUpdateDTO;
+import gr.aueb.cf.schoolapp.dto.*;
+import gr.aueb.cf.schoolapp.mapper.Mapper;
 import gr.aueb.cf.schoolapp.service.ITeacherService;
 import gr.aueb.cf.schoolapp.validator.ValidatorUtil;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -17,8 +16,10 @@ import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.UriInfo;
 import lombok.RequiredArgsConstructor;
 
+import javax.print.attribute.standard.Media;
 import java.net.URI;
 import java.util.List;
+import java.util.Map;
 
 
 @ApplicationScoped
@@ -37,7 +38,9 @@ public class TeacherRestController {
     @Path("")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response addTeacher(TeacherInsertDTO insertDTO, @Context UriInfo uriInfo) throws EntityInvalidArgumentException, EntityAlreadyExistsException {
+    public Response addTeacher(TeacherInsertDTO insertDTO, @Context UriInfo uriInfo)
+            throws EntityInvalidArgumentException, EntityAlreadyExistsException {
+
         List<String> errors = ValidatorUtil.validateDTO(insertDTO);
         if (!errors.isEmpty()) {
             throw new EntityInvalidArgumentException("Teacher", String.join("\n", errors));
@@ -55,7 +58,9 @@ public class TeacherRestController {
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response updateTeacher(@PathParam("teacherId") Long teacherId, TeacherUpdateDTO updateDTO) throws EntityInvalidArgumentException, EntityNotFoundException {
+    public Response updateTeacher(@PathParam("teacherId") Long teacherId, TeacherUpdateDTO updateDTO)
+            throws EntityInvalidArgumentException, EntityNotFoundException {
+
         List<String> errors = ValidatorUtil.validateDTO(updateDTO);
         if (!errors.isEmpty()) {
             throw new EntityInvalidArgumentException("Teacher", String.join("\n", errors));
@@ -66,4 +71,76 @@ public class TeacherRestController {
                 .entity(readOnlyDTO)
                 .build();
     }
+
+    @DELETE
+    @Path("/{teacherId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteTeacher(@PathParam("teacherId") Long teacherId)
+            throws EntityNotFoundException{
+
+        TeacherReadOnlyDTO readOnlyDTO = teacherService.getTeacherById(teacherId);
+         teacherService.deleteTeacher(teacherId);
+         return Response.status(Response.Status.OK)
+                 .entity(readOnlyDTO)
+                 .build();
+    }
+
+    @GET
+    @Path("/{teacherId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getTeacher(@PathParam("teacherId") Long id)
+            throws EntityNotFoundException{
+        TeacherReadOnlyDTO readOnlyDTO = teacherService.getTeacherById(id);
+        return Response.status(Response.Status.OK)
+                .entity(readOnlyDTO)
+                .build();
+    }
+
+    @GET
+    @Path("/filtered")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getFiltered(@QueryParam("firstname") @DefaultValue("") String firstname,
+                                @QueryParam("lastname") @DefaultValue("") String lastname,
+                                @QueryParam("vat") @DefaultValue("") String vat){
+       TeacherFiltersDTO filtersDTO = new TeacherFiltersDTO(firstname, lastname, vat);
+       Map<String, Object> criteria;
+
+       criteria = Mapper.mapToCriteria(filtersDTO);
+       List<TeacherReadOnlyDTO> readOnlyDTOs = teacherService.getTeachersByCriteria(criteria);
+
+       return Response.status(Response.Status.OK)
+               .entity(readOnlyDTOs)
+               .build();
+    }
+
+    public PaginatedResult<TeacherReadOnlyDTO> getFilteredPaginated(@QueryParam("firstname") @DefaultValue("") String firstname,
+                                                                    @QueryParam("lastname") @DefaultValue("") String lastname,
+                                                                    @QueryParam("vat") @DefaultValue("") String vat,
+                                                                    @QueryParam("page") @DefaultValue("") Integer page,
+                                                                    @QueryParam("size") @DefaultValue("") Integer size)
+            throws EntityInvalidArgumentException
+    {
+        TeacherFiltersDTO filtersDTO = new TeacherFiltersDTO(firstname, lastname, vat);
+        Map<String, Object> criteria;
+
+        criteria = Mapper.mapToCriteria(filtersDTO);
+
+        if(page<0) throw new EntityInvalidArgumentException("PageInvalidNumber", "Invalid page number");
+        if(size<=0) throw new EntityInvalidArgumentException("PageInvalidSize", "Invalid page size");
+
+        List<TeacherReadOnlyDTO> readOnlyDTOs = teacherService.getTeachersByCriteriaPaginated(criteria, size, page);
+        long totalItems = teacherService.getTeachersCountByCriteria(criteria);
+        int totalPages = (int) Math.ceil( (double) totalItems/size);
+
+        return new PaginatedResult<>(
+                readOnlyDTOs,
+                page,
+                size,
+                totalPages,
+                totalItems
+        );
+
+
+    }
+
 }
