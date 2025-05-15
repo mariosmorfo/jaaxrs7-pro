95652bc94cc5ea7b41bd33e00271e0b15a0927c9
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-05-15 19:16:11 +0300

    feat: Exception Mapper created

---
+++ src/main/java/gr/aueb/cf/schoolapp/core/services/AppExceptionMapper.java
@@ -0,0 +1,31 @@
+package gr.aueb.cf.schoolapp.core.services;
+
+import gr.aueb.cf.schoolapp.core.exceptions.*;
+import gr.aueb.cf.schoolapp.dto.ResponseMessageDTO;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+
+import java.lang.annotation.Repeatable;
+
+public class AppExceptionMapper implements ExceptionMapper<EntityGenericException>  {
+    @Override
+    public Response toResponse(EntityGenericException exception) {
+        Response.Status status = Response.Status.INTERNAL_SERVER_ERROR;
+
+        if (exception instanceof EntityNotFoundException) {
+            status = Response.Status.NOT_FOUND;
+        } else if (exception instanceof EntityInvalidArgumentException) {
+            status = Response.Status.BAD_REQUEST;
+        } else if (exception instanceof EntityNotAuthorizedException) {
+            status = Response.Status.UNAUTHORIZED;
+        } else if (exception instanceof EntityAlreadyExistsException) {
+            status = Response.Status.CONFLICT;
+        } else if (exception instanceof AppServerException) {
+            status = Response.Status.SERVICE_UNAVAILABLE;
+        }
+        return Response.status(status).entity(new ResponseMessageDTO(exception.getMessage()))
+                .type(MediaType.APPLICATION_JSON)
+                .build();
+    }
+}
