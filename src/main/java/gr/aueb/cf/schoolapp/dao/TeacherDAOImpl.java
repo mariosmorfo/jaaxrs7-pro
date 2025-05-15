ca4c97924200ab9f782e0a2636a85e1a04ddacd5
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-05-15 20:16:59 +0300

    fix: class extends

--- src/main/java/gr/aueb/cf/schoolapp/dao/TeacherDAOImpl.java
+++ src/main/java/gr/aueb/cf/schoolapp/dao/TeacherDAOImpl.java
@@ -4,7 +4,7 @@ import gr.aueb.cf.schoolapp.model.Teacher;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-public class TeacherDAOImpl extends AbstractDAOBack<Teacher> implements ITeacherDAO{
+public class TeacherDAOImpl extends AbstractDAO<Teacher> implements ITeacherDAO{
     public TeacherDAOImpl(){
         this.setPersistenceClass(Teacher.class);
     }
