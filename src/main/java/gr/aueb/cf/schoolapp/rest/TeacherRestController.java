590a3fb45ae389e73433270fcbcc962fa4888d3e
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-05-15 19:32:35 +0300

    feat: Add teacher Update Controller

--- src/main/java/gr/aueb/cf/schoolapp/rest/TeacherRestController.java
+++ src/main/java/gr/aueb/cf/schoolapp/rest/TeacherRestController.java
@@ -1,17 +1,23 @@
 package gr.aueb.cf.schoolapp.rest;
 
+import gr.aueb.cf.schoolapp.core.exceptions.EntityAlreadyExistsException;
 import gr.aueb.cf.schoolapp.core.exceptions.EntityInvalidArgumentException;
+import gr.aueb.cf.schoolapp.core.exceptions.EntityNotFoundException;
 import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
+import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
+import gr.aueb.cf.schoolapp.dto.TeacherUpdateDTO;
 import gr.aueb.cf.schoolapp.service.ITeacherService;
 import gr.aueb.cf.schoolapp.validator.ValidatorUtil;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.Path;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.UriInfo;
 import lombok.RequiredArgsConstructor;
 
+import java.net.URI;
 import java.util.List;
 
 
@@ -27,11 +33,37 @@ public class TeacherRestController {
     public TeacherRestController(ITeacherService teacherService) {
         this.teacherService = teacherService;
     }
-
-    public Response addTeacher(TeacherInsertDTO insertDTO, @Context UriInfo uriInfo) throws EntityInvalidArgumentException{
+    @POST
+    @Path("")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response addTeacher(TeacherInsertDTO insertDTO, @Context UriInfo uriInfo) throws EntityInvalidArgumentException, EntityAlreadyExistsException {
         List<String> errors = ValidatorUtil.validateDTO(insertDTO);
         if (!errors.isEmpty()) {
             throw new EntityInvalidArgumentException("Teacher", String.join("\n", errors));
         }
+
+        TeacherReadOnlyDTO readOnlyDTO = teacherService.insertTeacher(insertDTO);
+        URI newResourceUri = uriInfo.getAbsolutePathBuilder()
+                .path(String.valueOf(readOnlyDTO.getId()))
+                .build();
+
+        return Response.created(newResourceUri).entity(readOnlyDTO).build();
+    }
+
+    @Path("/{teacherId}")
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response updateTeacher(@PathParam("teacherId") Long teacherId, TeacherUpdateDTO updateDTO) throws EntityInvalidArgumentException, EntityNotFoundException {
+        List<String> errors = ValidatorUtil.validateDTO(updateDTO);
+        if (!errors.isEmpty()) {
+            throw new EntityInvalidArgumentException("Teacher", String.join("\n", errors));
+        }
+
+        TeacherReadOnlyDTO readOnlyDTO = teacherService.updateTeacher(updateDTO);
+        return Response.status(Response.Status.OK)
+                .entity(readOnlyDTO)
+                .build();
     }
 }
